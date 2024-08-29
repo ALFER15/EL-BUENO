0323677a4adba538cf4324584ddd74efd7d1955e
ALFER15 <aarias22@alumnos.uaq.mx>
2024-08-29 15:23:39 -0600

    Se modificado posiciones, se la funcionalidad de las alertas se reparo

--- resources/views/product/index-producto.blade.php
+++ resources/views/product/index-producto.blade.php
@@ -1,9 +1,15 @@
 <x-app-layout>
-    <div class="text-4xl font-extrabold text-blue-600 dark:text-white mb-4">Listado de productos</div>
+    @if(session('success'))
+        <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+            <span class="font-medium">Alerta:</span> {{ session('success') }}
+        </div>
+    @endif
+
+    <div class="text-4xl font-extrabold text-blue-600 dark:text-white mb-12 mt-12 text-center">Listado de productos</div>
 
     <div class="flex justify-end mb-4">
         <button type="button" class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">
-            <a href="{{route('producto.create')}}">Crear</a>
+            <a href="{{ route('producto.create') }}">Crear</a>
         </button>
     </div>
 
@@ -21,13 +27,13 @@
                 @foreach($products as $product)
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                        <a href="{{route('producto.show', $product->id)}}" class="hover:underline">{{$product->name}}</a>
+                        <a href="{{ route('producto.show', $product->id) }}" class="hover:underline">{{ $product->name }}</a>
                     </th>
                     <td class="px-6 py-4">
-                        <a href="/show-producto" class="hover:underline">{{$product->desc}}</a>
+                        <a href="/show-producto" class="hover:underline">{{ $product->desc }}</a>
                     </td>
                     <td class="px-6 py-4">
-                        <a href="/show-producto" class="hover:underline">{{$product->price}}</a>
+                        <a href="/show-producto" class="hover:underline">{{ $product->price }}</a>
                     </td>
                     <td class="px-6 py-4 text-right">
                         <a href="{{ route('producto.update', ['product' => $product->id]) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
@@ -39,6 +45,6 @@
     </div>
 
     <div class="mt-4">
-        {{$products->links()}}
+        {{ $products->links() }}
     </div>
 </x-app-layout>
--- resources/views/product/show-producto.blade.php
+++ resources/views/product/show-producto.blade.php
@@ -4,13 +4,17 @@
         <a href="{{ route('producto.create') }}" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Crear</a>
     </div>
 
-
     <div class="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 pt-16">
         <div class="p-6 bg-white dark:bg-gray-800 rounded-lg w-full max-w-2xl shadow-lg">
+            @if(session('edit'))
+                <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+                    <span class="font-medium">Alerta!</span> {{ session('edit') }}
+                </div>
+            @endif
+
             <h1 class="text-3xl font-bold text-center mb-6 text-gray-900 dark:text-white">Detalles del Producto</h1>
 
             <div class="grid gap-4">
-
                 <div class="bg-blue-50 dark:bg-gray-700 p-4 rounded-lg">
                     <p class="text-lg font-semibold text-gray-800 dark:text-gray-300">Número de producto:</p>
                     <p class="text-blue-500 text-lg">{{ $productDetail->product_number }}</p>
